03c7893c31cb1306e8a13c07d7fdc0ac539efa57
Sahil Kewat <147237807+Sahilkewat80085@users.noreply.github.com>
2024-02-02 10:43:59 +0530

    Add files via upload

---
+++ game/scoreboard.py
@@ -0,0 +1,28 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 260)
+        self.hideturtle()
+        self.update_score()
+
+    def update_score(self):
+        self.write(f"Score {self.score}", align="center", font=("Arial", 24, "normal"))
+
+    def game_over(self):
+        self.goto((0, 0))
+        self.write("GAME OVER", align="center", font=("Arial", 50, "normal"))
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.write(f"Score {self.score}", align="center", font=("Arial", 24, "normal"))
+
+
+
